2adad9446c19305ed2d00b27387ab4c977a9ed0a
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2022-02-27 22:56:12 -0500

    dynamic listings and about page

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    return render(request, 'listings/listings.html', {
+        'listings': paged_listings
+    })
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template.response import TemplateResponse
+
+from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
-    args = {}
-    fruits_list = ["apple", "banana", "mango"]
-    fruits_list.append("orange")
-    fruits_list.append("grapes")
-    args['fruits_list'] = fruits_list
-    #return render(request, 'pages/index.html')
-    return TemplateResponse(request, 'pages/index.html', args)
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
